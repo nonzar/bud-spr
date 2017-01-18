9007d03b91f8893ab7b1f1c74b7793239000292a
nonzar <dwljia@qq.com>
2017-01-19 04:22:05 +0800

    edit by nz

--- src/js/ptl_spr_1.js
+++ src/js/ptl_spr_1.js
@@ -1,67 +1,88 @@
-var app = angular.module("app", ['ui.grid', 'ui.grid.pagination']);
-app.controller('MainCtrl', ['$scope', '$http', 'uiGridConstants', function ($scope, $http, uiGridConstants) {
-    var paginationOptions = {
-        pageNumber: 1,
-        pageSize: 5,
-        sort: null
-    };
-    $scope.gridOptions = {
-        paginationPageSizes: [5],
-        paginationPageSize: 5,
-        useExternalPagination: true,
-        useExternalSorting: true,
-        columnDefs: [
-            {name: '促销员id', enableSorting: false},
-            {name: '验证码', enableSorting: false},
-            {name: '大区', enableSorting: false},
-            {name: '城市', enableSorting: false},
-            {name: '城市等级', enableSorting: false},
-            {name: '促销员名称', enableSorting: false},
-            {name: '渠道', enableSorting: false},
-            {name: 'PTL', enableSorting: false},
-            {name: '状态', enableSorting: false},
-            {name: '操作', enableSorting: false}
-        ],
-        onRegisterApi: function (gridApi) {
-            $scope.gridApi = gridApi;
-            $scope.gridApi.core.on.sortChanged($scope, function (grid, sortColumns) {
-                if (sortColumns.length == 0) {
-                    paginationOptions.sort = null;
-                } else {
-                    paginationOptions.sort = sortColumns[0].sort.direction;
-                }
-                getPage();
-            });
-            gridApi.pagination.on.paginationChanged($scope, function (newPage, pageSize) {
-                paginationOptions.pageNumber = newPage;
-                paginationOptions.pageSize = pageSize;
-                getPage();
+var ctrl = {
+    $scope: null,
+    pagination: {},
+    reset: function () {
+        ctrl.pagination = {
+            totalPages: -1,
+            sort: 0,
+            page: 1
+        };
+    },
+    getPage: function () {
+        if (!ctrl.$scope.ths) {
+            ctrl.$scope.$apply(function () {
+                ctrl.$scope.ths = [
+                    // "#",
+                    "促销员id",
+                    "验证码",
+                    "大区",
+                    "城市",
+                    "城市等级",
+                    "促销员名称",
+                    "渠道",
+                    "PTL",
+                    "状态",
+                    "操作"
+                ];
             });
         }
-    };
-    var getPage = function () {
-        console.log(uiGridConstants);
-        $.post("http://120.77.53.178/baiwei/baiweistat.php/home/index/qspr", {
-            spr: $(".wbSearch input[type='text']").val() || 862094
-        }, function (data) {
+        $.post("http://120.77.53.178/baiwei/baiweistat.php/home/index/qspr", function (_data) {
+            _data = {
+                sort: ctrl.pagination.sort,
+                page: ctrl.pagination.page,
+                spr: $(".wbSearch input[type='text']").val()
+            };
+            console.log(_data);
+            return _data;
+        }({}), function (data) {
             console.log(data = JSON.parse(data));
-            $scope.gridOptions.totalItems = data.data.totalpages - 1;
-            $scope.gridOptions.data = function (data, arr) {
-                    arr.push({
-                        "促销员id": data.data.id,
-                        "验证码": data.data.code,
-                        "大区": data.data.region,
-                        "城市": data.data.city,
-                        "城市等级": data.data.type,
-                        "促销员名称": data.data.name,
-                        "渠道": data.data.vendor,
-                        "PTL": data.data.ptl,
-                        "状态": data.data.isover,
-                        "操作": "修改"
-                    });
-                return arr;
-            }(data, []);
+            ctrl.pagination.totalPages = parseInt(data.data.totalpages);
+            ctrl.$scope.$apply(function () {
+                ctrl.$scope.tds = [data.data];
+            });
         });
-    };
-    getPage();
-}]);
+    },
+    getPrevPage: function () {
+        if (ctrl.pagination.totalPages == -1) {
+            return;
+        }
+        if (ctrl.pagination.page <= 1) {
+            alert("已经是第一页。");
+            return;
+        }
+        ctrl.pagination.page--;
+        ctrl.getPage();
+    },
+    getNextPage: function () {
+        if (ctrl.pagination.totalPages == -1) {
+            return;
+        }
+        else if (ctrl.pagination.page >= ctrl.pagination.totalPages) {
+            alert("已经是最后一页。");
+            return;
+        }
+        ctrl.pagination.page++;
+        ctrl.getPage();
+    }
+};
+var app = angular.module('app', []);
+app.controller('customersCtrl', function ($scope) {
+    ctrl.$scope = $scope;
+    $scope.ths = [
+        // "#",
+        "促销员id",
+        "验证码",
+        "大区",
+        "城市",
+        "城市等级",
+        "促销员名称",
+        "渠道",
+        "PTL",
+        "状态",
+        "操作"
+    ];
+});
+$(".wbSearch button").on("click", function () {
+    ctrl.reset();
+    ctrl.getPage(ctrl.$scope);
+});
--- src/js/ptl_spr_2.js
+++ src/js/ptl_spr_2.js
@@ -1,67 +1,83 @@
-var app = angular.module("app", ['ui.grid', 'ui.grid.pagination']);
-app.controller('MainCtrl', ['$scope', '$http', 'uiGridConstants', function ($scope, $http, uiGridConstants) {
-    var paginationOptions = {
-        pageNumber: 1,
-        pageSize: 5,
-        sort: null
-    };
-    $scope.gridOptions = {
-        paginationPageSizes: [5],
-        paginationPageSize: 5,
-        useExternalPagination: true,
-        useExternalSorting: true,
-        columnDefs: [
-            {name: '#'},
-            {name: '促销员id', enableSorting: false},
-            {name: '大区', enableSorting: false},
-            {name: '城市', enableSorting: false},
-            {name: '城市等级', enableSorting: false},
-            {name: '促销员名称', enableSorting: false},
-            {name: '渠道', enableSorting: false},
-            {name: 'PTL', enableSorting: false},
-            {name: '状态', enableSorting: false}
-        ],
-        onRegisterApi: function (gridApi) {
-            $scope.gridApi = gridApi;
-            $scope.gridApi.core.on.sortChanged($scope, function (grid, sortColumns) {
-                if (sortColumns.length == 0) {
-                    paginationOptions.sort = null;
-                } else {
-                    paginationOptions.sort = sortColumns[0].sort.direction;
-                }
-                getPage();
-            });
-            gridApi.pagination.on.paginationChanged($scope, function (newPage, pageSize) {
-                paginationOptions.pageNumber = newPage;
-                paginationOptions.pageSize = pageSize;
-                getPage();
+var ctrl = {
+    $scope: null,
+    pagination: {},
+    reset: function () {
+        ctrl.pagination = {
+            totalPages: -1,
+            sort: 0,
+            page: 1
+        };
+    },
+    getPage: function () {
+        if (!ctrl.$scope.ths) {
+            ctrl.$scope.$apply(function () {
+                ctrl.$scope.ths = [
+                    // "#",
+                    '促销员id',
+                    '大区',
+                    '城市',
+                    '城市等级',
+                    '促销员名称',
+                    '渠道',
+                    'PTL',
+                    '状态'
+                ];
             });
         }
-    };
-    var getPage = function () {
-        console.log(uiGridConstants);
         $.post("http://120.77.53.178/baiwei/baiweistat.php/home/index/qptl", {
-            ptl: 0,//*
-            page: paginationOptions.pageNumber
+            ptl: 123,//*
+            page: ctrl.page
         }, function (data) {
             console.log(data = JSON.parse(data));
-            $scope.gridOptions.totalItems = data.data.totalpages - 1;
-            $scope.gridOptions.data = function (data, arr) {
-                for (var i = 0; i < data.data.data.length; i++) {
-                    arr.push({
-                        "#": i + 1,
-                        "促销员id": data.data.data[i].id,
-                        "大区城市": data.data.data[i].region,
-                        "城市等级": data.data.data[i].type,
-                        "促销员名称": data.data.data[i].name,
-                        "渠道": data.data.data[i].vendor,
-                        "PTL": data.data.data[i].ptl,
-                        "状态": data.data.data[i].isover
-                    });
-                }
-                return arr;
-            }(data, []);
+            ctrl.pagination.totalPages = parseInt(data.data.totalpages);
+            ctrl.$scope.$apply(function () {
+                ctrl.$scope.tds = data.data.data;
+            });
         });
-    };
-    getPage();
-}]);
+    },
+    getPrevPage: function () {
+        if (ctrl.pagination.totalPages == -1) {
+            return;
+        }
+        if (ctrl.pagination.page <= 1) {
+            alert("已经是第一页。");
+            return;
+        }
+        ctrl.pagination.page--;
+        ctrl.getPage();
+    },
+    getNextPage: function () {
+        if (ctrl.pagination.totalPages == -1) {
+            return;
+        }
+        else if (ctrl.pagination.page >= ctrl.pagination.totalPages) {
+            alert("已经是最后一页。");
+            return;
+        }
+        ctrl.pagination.page++;
+        ctrl.getPage();
+    }
+};
+var app = angular.module('app', []);
+app.controller('customersCtrl', function ($scope) {
+    ctrl.$scope = $scope;
+    $scope.ths = [
+        // "#",
+        '促销员id',
+        '大区',
+        '城市',
+        '城市等级',
+        '促销员名称',
+        '渠道',
+        'PTL',
+        '状态'
+    ];
+    ctrl.reset();
+    ctrl.getPage(ctrl.$scope);
+});
+$(".wbTable .btn-prev").on("click", function () {
+    ctrl.getPrevPage();
+});
+$(".wbTable .btn-next").on("click", function () {
+    ctrl.getNextPage();
+});
