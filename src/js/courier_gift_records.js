3b2d30bb7a679beb14e68a88511b474db708b2e5
nonzar <dwljia@qq.com>
2017-01-19 05:55:27 +0800

    edit by nz

--- src/js/courier_gift_records.js
+++ src/js/courier_gift_records.js
@@ -1,69 +1,83 @@
-var none = "/";
-var app = angular.module("app", ['ui.grid', 'ui.grid.pagination']);
-app.controller('MainCtrl', ['$scope', '$http', 'uiGridConstants', function ($scope, $http, uiGridConstants) {
-    var paginationOptions = {
-        pageNumber: 1,
-        pageSize: 5,
-        sort: null
-    };
-    $scope.gridOptions = {
-        paginationPageSizes: [5],
-        paginationPageSize: 5,
-        useExternalPagination: true,
-        useExternalSorting: true,
-        columnDefs: [
-            // {name: '#'},
-            {name: '记录id'},
-            {name: 'openid', enableSorting: false},
-            {name: '产品名称', enableSorting: false},
-            {name: '兑奖时间', enableSorting: false},
-            {name: '姓名', enableSorting: false},
-            {name: '电话', enableSorting: false},
-            {name: '快递单号', enableSorting: false},
-            {name: '地址', enableSorting: false}
-        ],
-        onRegisterApi: function (gridApi) {
-            $scope.gridApi = gridApi;
-            $scope.gridApi.core.on.sortChanged($scope, function (grid, sortColumns) {
-                if (sortColumns.length == 0) {
-                    paginationOptions.sort = null;
-                } else {
-                    paginationOptions.sort = sortColumns[0].sort.direction;
-                }
-                getPage();
-            });
-            gridApi.pagination.on.paginationChanged($scope, function (newPage, pageSize) {
-                paginationOptions.pageNumber = newPage;
-                paginationOptions.pageSize = pageSize;
-                getPage();
+var ctrl = {
+    $scope: null,
+    pagination: {},
+    reset: function () {
+        ctrl.pagination = {
+            totalPages: -1,
+            sort: 0,
+            page: 1
+        };
+    },
+    getPage: function () {
+        if (!ctrl.$scope.ths) {
+            ctrl.$scope.$apply(function () {
+                ctrl.$scope.ths = [
+                    // {name: '#'},
+                    '记录id',
+                    'openid',
+                    '产品名称',
+                    '兑奖时间',
+                    '姓名',
+                    '电话',
+                    '快递单号',
+                    '地址'
+                ];
             });
         }
-    };
-    var getPage = function () {
-        console.log(uiGridConstants);
         $.post("http://120.77.53.178/baiwei/baiweistat.php/home/index/qaward", {
             type: 0,
-            page: paginationOptions.pageNumber
+            page: ctrl.pagination.page
         }, function (data) {
             console.log(data = JSON.parse(data));
-            $scope.gridOptions.totalItems = data.data.totalpages - 1;
-            $scope.gridOptions.data = function (data, arr) {
-                for (var i = 0; i < data.data.data.length; i++) {
-                    arr.push({
-                        // "#": i + 1,
-                        "记录id": data.data.data[i].id,
-                        "openid": data.data.data[i].openid,
-                        "产品名称": none,
-                        "兑奖时间": data.data.data[i].act_time,
-                        "姓名": none,
-                        "电话": none,
-                        "快递单号": data.data.data[i].package,
-                        "地址": none
-                    });
-                }
-                return arr;
-            }(data, []);
+            ctrl.pagination.totalPages = parseInt(data.data.totalpages);
+            ctrl.$scope.$apply(function () {
+                ctrl.$scope.tds = data.data.data;
+            });
         });
-    };
-    getPage();
-}]);
+    },
+    getPrevPage: function () {
+        if (ctrl.pagination.totalPages == -1) {
+            return;
+        }
+        if (ctrl.pagination.page <= 1) {
+            alert("已经是第一页。");
+            return;
+        }
+        ctrl.pagination.page--;
+        ctrl.getPage();
+    },
+    getNextPage: function () {
+        if (ctrl.pagination.totalPages == -1) {
+            return;
+        }
+        else if (ctrl.pagination.page >= ctrl.pagination.totalPages) {
+            alert("已经是最后一页。");
+            return;
+        }
+        ctrl.pagination.page++;
+        ctrl.getPage();
+    }
+};
+var app = angular.module('app', []);
+app.controller('customersCtrl', function ($scope) {
+    ctrl.$scope = $scope;
+    $scope.ths = [
+        // {name: '#'},
+        '记录id',
+        'openid',
+        '产品名称',
+        '兑奖时间',
+        '姓名',
+        '电话',
+        '快递单号',
+        '地址'
+    ];
+    ctrl.reset();
+    ctrl.getPage(ctrl.$scope);
+});
+$(".wbTable .btn-prev").on("click", function () {
+    ctrl.getPrevPage();
+});
+$(".wbTable .btn-next").on("click", function () {
+    ctrl.getNextPage();
+});
--- src/js/mkt_gift_inventory.js
+++ src/js/mkt_gift_inventory.js
@@ -1,36 +1,68 @@
-var none = "/";
-var app = angular.module("app", ['ui.grid', 'ui.grid.pagination']);
-app.controller('MainCtrl', ['$scope', '$http', 'uiGridConstants', function ($scope, $http, uiGridConstants) {
-    $scope.gridOptions = {
-        columnDefs: [
-            // {name: '#'},
-            {name: '记录id'},
-            {name: 'openid', enableSorting: false},
-            {name: '产品名称', enableSorting: false},
-            {name: '兑奖时间', enableSorting: false},
-            {name: '姓名', enableSorting: false},
-            {name: '电话', enableSorting: false},
-            {name: '快递单号', enableSorting: false},
-            {name: '地址', enableSorting: false}
-        ]
-    };
-    $.post("http://120.77.53.178/baiwei/baiweistat.php/home/index/qobj", {}, function (data) {
-        console.log(data = JSON.parse(data));
-        $scope.gridOptions.data = function (data, arr) {
-            for (var i = 0; i < data.data.length; i++) {
-                arr.push({
-                    // "#": i + 1,
-                    "记录id": data.data[i].id,
-                    "openid": data.data[i].openid,
-                    "产品名称": none,
-                    "兑奖时间": data.data[i].act_time,
-                    "姓名": none,
-                    "电话": none,
-                    "快递单号": data.data[i].package,
-                    "地址": none
-                });
-            }
-            return arr;
-        }(data, []);
-    });
-}]);
+var ctrl = {
+    $scope: null,
+    pagination: {},
+    reset: function () {
+        ctrl.pagination = {
+            totalPages: -1,
+            sort: 0,
+            page: 1
+        };
+    },
+    getPage: function () {
+        if (!ctrl.$scope.ths) {
+            ctrl.$scope.$apply(function () {
+                ctrl.$scope.ths = [
+                    '奖品Type',
+                    '名称',
+                    '兑换积分',
+                    '总量',
+                    '兑换数量',
+                    '兑换率'
+                ];
+            });
+        }
+        $.post("http://120.77.53.178/baiwei/baiweistat.php/home/index/qobj", {}, function (data) {
+            console.log(data = JSON.parse(data));
+            ctrl.pagination.totalPages = parseInt(data.data.totalpages);
+            ctrl.$scope.$apply(function () {
+                ctrl.$scope.tds = data.data;
+            });
+        });
+    },
+    getPrevPage: function () {
+        if (ctrl.pagination.totalPages == -1) {
+            return;
+        }
+        if (ctrl.pagination.page <= 1) {
+            alert("已经是第一页。");
+            return;
+        }
+        ctrl.pagination.page--;
+        ctrl.getPage();
+    },
+    getNextPage: function () {
+        if (ctrl.pagination.totalPages == -1) {
+            return;
+        }
+        else if (ctrl.pagination.page >= ctrl.pagination.totalPages) {
+            alert("已经是最后一页。");
+            return;
+        }
+        ctrl.pagination.page++;
+        ctrl.getPage();
+    }
+};
+var app = angular.module('app', []);
+app.controller('customersCtrl', function ($scope) {
+    ctrl.$scope = $scope;
+    $scope.ths = [
+        '奖品Type',
+        '名称',
+        '兑换积分',
+        '总量',
+        '兑换数量',
+        '兑换率'
+    ];
+    ctrl.reset();
+    ctrl.getPage(ctrl.$scope);
+});
